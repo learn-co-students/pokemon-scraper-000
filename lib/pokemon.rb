62893091aa9f2197f5ed9a480f7123a9098caea0
OnlyHughMan <hughvidler@hotmail.com>
2017-06-26 03:05:59 +1000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,57 @@
 class Pokemon
+
+	DEFAULT_HP = 60
+
+	attr_accessor :id, :name, :type, :db, :hp
+
+	def initialize(data = [])
+		@db = data
+	end
+
+
+	def self.save(name, type, db)
+		command = 
+			<<-SQL
+				INSERT INTO pokemon (name, type) VALUES (?,?);
+			SQL
+		db.execute(command, name, type)
+	end
+
+	def self.find(id, db)
+		command = <<-SQL
+      	SELECT * FROM pokemon WHERE id = ?;
+    	SQL
+
+    	db
+
+    	pokedata = db.execute(command, id)
+    	id = pokedata[0][0]
+    	name = pokedata[0][1]
+    	type = pokedata[0][2]
+    	hp = pokedata[0][3]
+
+    	Pokemon.new.tap do |pokemon|
+     		pokemon.id = id
+      	pokemon.name = name
+      	pokemon.type = type
+      	pokemon.db = db
+      	pokemon.hp = hp
+		end
+
+	end
+
+  def update
+    sql = <<-SQL
+        UPDATE pokemons
+        SET name = ?, type = ?, hp = ?
+        WHERE id = ?;
+    SQL
+
+    self.db.execute(sql, self.name, self.type, self.hp, self.id)
+  end
+
+
+
+
+
 end
